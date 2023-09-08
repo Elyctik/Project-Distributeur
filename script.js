cc1d2b2543b03c72d3819e98d743eb9489eae62b
Geoffrey <geoffreybeyer@hotmail.com>
2023-09-08 11:15:14 +0200

    Input user (+Regex) v2

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+function verify(texteUtilisateur){
+    let regex = new RegExp("^[abc]0[123]$","gmi");
+
+       if (regex.test(texteUtilisateur)){
+       return true;
+       }
+       else{
+       return false;
+       }
+}
+
 function insererTexte() {
     // Récupérez la valeur de l'input texte
     let texteUtilisateur = document.getElementById("texteUtilisateur").value;
 
     // Récupérez l'élément où vous souhaitez afficher le texte (dans cet exemple, c'est un paragraphe avec l'id "affichageTexte")
     let affichageTexte = document.getElementById("affichageTexte");
-    let regex = new RegExp("^[abc][123]$","gmi");
+    let regex = new RegExp("^[abc]0[123]$","gmi");
 
        if (regex.test(texteUtilisateur)){
        // Affichez le texte dans l'élément souhaité
@@ -13,4 +24,5 @@ function insererTexte() {
        }
        else{
         affichageTexte.innerHTML = "Wrong input";
+       }
        }
