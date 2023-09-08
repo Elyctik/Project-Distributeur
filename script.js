74b0c480f5f142ff37bba67f415578cf0adee96a
Aline Daems <daems.aline90@gmail.com>
2023-09-08 11:17:41 +0200

    afficher le contenu

--- script.js
+++ script.js
@@ -15,15 +15,76 @@ function insererTexte() {
     }
 }
 
-function afficher(){
+function afficherBoisson(){
 
     let coca = document.getElementById("A1");
 
+    let quantiteCoca = 10;
+
     coca.textContent = "Coca";
 
-    alert("Il y a 1 coca")
+    let fanta = document.getElementById("A2");
+
+    let quantitefanta = 10;
+
+    fanta.textContent = "Fanta";
+
+    let sprite = document.getElementById("A3");
+
+    let quantitesprite = 10;
+
+    sprite.textContent = "Sprite";
+
+    alert("Il y a " + quantiteCoca + " Coca" + " Il y a " + quantitefanta + " Fanta" + "Il y a " + quantitesprite + " sprite")
 }
 
+
+function afficherBonbon(){
+
+    let haribo = document.getElementById("B1");
+
+    let quantiteHaribo = 10;
+
+    haribo.textContent = "Haribo"
+
+    let tagada = document.getElementById("B2")
+
+    let quantitetagada = 10;
+
+    tagada.textContent = "tagada"
+
+    let nounours = document.getElementById("B3")
+
+    let quantiteNounours = 10;
+
+    nounours.textContent = "Nounours"
+
+    alert("il y a " + quantiteHaribo + " haribo " + " il y a " + quantitetagada + "tagadas" + " il y a " + quantiteNounours + " Nounours")
+
+}
+
+function afficherChips(){
+    let sel = document.getElementById("C1");
+
+    let quantiteSel = 10;
+
+    sel.textContent = "Chips au sel"
+
+    let paprika = document.getElementById("C2")
+
+    let quantitePaprika = 10;
+
+    paprika.textContent = "Chips au Paprika"
+
+    let pikkles = document.getElementById("C3")
+
+    let quantitePikkles = 10;
+
+    pikkles.textContent = "Chips Pikkles"
+
+    alert("il y a " + quantiteSel + " chips au sel " + " il y a " + quantitePaprika + " chips au paprika" + " il y a " + quantitePikkles + " chips au pikkles")
+
+}
 let distributeur = new Array()[3][3]
 
 function refill(){
